edb24bfceb88ee551843e92439baf717e25d40a8
Анна Овчаренко <ovcharenko.anna1420@gmail.com>
2026-09-18 09:52:46 +0300

    yeah

---
+++ src/lab_01/03_discounts_vat.py
@@ -0,0 +1,7 @@
+price, discount, vat = int(input('price: ')), int(input('discount: ')), int(input('vat: '))
+base = price * (1 - discount/100)
+vat_amount = base * (vat/100)
+total = base + vat_amount
+print(f'База после скидки: {base:.2f} ₽')
+print(f'НДС:               {vat_amount:.2f} ₽')
+print(f'Итого к оплате:    {total:.2f} ₽')
